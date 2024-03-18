d52863dc5efa6b221c0d854688111cb896238d75
Ning114 <david.leng114@gmail.com>
2024-03-18 13:05:34 -0700

    Added ammo pickup system

---
+++ Assets/Scripts/ammo.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ammo : MonoBehaviour
+{
+
+    public int ammoAmount = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("\nDetected collision with ammoBox");
+        if (other.tag == "Player") {
+            Debug.Log("\nDetected Player, consuming ammo!");
+            PlayerMovement.instance.currentAmmo += ammoAmount;
+            Destroy(gameObject);
+        }
+    }
+
+}
